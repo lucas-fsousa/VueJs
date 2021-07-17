c37e079ebebf811db656d77c55baed197aa62d9d
Lucas de Sousa <Lucasads18@outlook.com>
2021-07-17 15:41:21 -0300

    new att

--- 2_project_vue/src/App.vue
+++ 2_project_vue/src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>
+  <div> <!-- COMPONENTES UTILIZADOS NO TEMPLATE -->
     <h1>Olá Mundo</h1>
     <PrimeiroComponente />
+    <LifeCycle />
     <p>Isso é o que? {{testando}}</p>
   </div>
 </template>
 <script>
+/*Passo 1: Importar o componente*/
   import PrimeiroComponente from "./components/PrimeiroComponente.vue"
+  import LifeCycle from "./components/LifeCycle.vue"
+  
   export default {
     name: 'App',
     components: {
-      PrimeiroComponente
+      /*Passo 2: Declarar o componente na área de componentes*/
+      PrimeiroComponente,
+      LifeCycle
     },
     data(){
       return {
---
+++ 2_project_vue/src/components/LifeCycle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Meu nome é: {{nome}}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "",
+    data(){
+      return {
+        nome: "Ainda não sei o nome"
+      }
+    },
+    created(){
+      setTimeout(() =>{
+        this.nome = "José TENSO"
+      }, 2000)
+    },
+    mounted(){
+      setTimeout(() =>{
+        this.nome = "José menos tenso"
+      }, 4000)
+    }
+  }
+
+</script>
