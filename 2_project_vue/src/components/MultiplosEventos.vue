aba1ba001264db5c4ac3c957557efaf015cba44d
Lucas de Sousa <Lucasads18@outlook.com>
2021-07-18 01:34:09 -0300

    new att

---
+++ 2_project_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="primeiro($event), segundo($event), terceiro($event)">Ativar Multiplos Eventos</button>
+    <p>{{multiplos_eventos}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MultiplosEventos",
+    data(){
+      return {
+        multiplos_eventos: ""
+      }
+    },
+    methods: {
+      primeiro(e){
+        this.multiplos_eventos = "Primeiro evento OK!";
+      },
+      segundo(e){
+        setTimeout(() => this.multiplos_eventos = "segundo EVENTO!", 1000);
+      },
+      terceiro(e){
+        setTimeout(() => this.multiplos_eventos = "", 2000);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    margin: 3%;
+  }
+</style>
--- 2_project_vue/src/components/PrimeiroComponente.vue
+++ 2_project_vue/src/components/PrimeiroComponente.vue
@@ -3,11 +3,16 @@
     <h1>Olá Vue!</h1>
     <p>Meu nome é: {{nome}}</p>
     <p>Profissão atual: {{profissao}}</p>
+    <MultiplosEventos />
   </div>
 </template>
 <script>
+import MultiplosEventos from "./MultiplosEventos.vue"
 export default {
   name: "PrimeiroComponente",
+  components: {
+    MultiplosEventos
+  },
   data(){
     return {
       nome: "Lucas",
